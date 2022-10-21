87b3ccb8acc612c240c8a299099490354fa4158a
Shaik Fathima Bi <skfathimabi@outlook.com>
2022-10-21 18:30:54 +0530

    Employee Added

--- src/main/java/com/restapi/rest/EmpResource.java
+++ src/main/java/com/restapi/rest/EmpResource.java
@@ -1,5 +1,8 @@
 package com.restapi.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,13 +13,19 @@ public class EmpResource
 {
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public Emp getEmployee()
+	public List<Emp> getEmployee()
 	{
 		System.out.println("getEmployee() called..............");
+		List<Emp> list= new ArrayList<Emp>();
 		Emp e1=new Emp();
 		e1.setName("Shaik Fathima");
 		e1.setEid(70840897);
 		
-		return e1;
+		Emp e2=new Emp();
+		e2.setName("Swathi");
+		e2.setEid(81095678);
+		list.add(e1);
+		list.add(e2);
+		return list;
 	}
 }
